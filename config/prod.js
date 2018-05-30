a3841f8dfe64086bf3856a2a2576323849a930e8
robabby <robabby23@gmail.com>
2018-05-30 10:47:02 -0500

    Prod keys

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,11 @@
 module.exports = {
-  // robabby83@gmail.com
-  // https://console.developers.google.com/apis/dashboard?project=nootristack-prod&duration=PT1H
   // googleClientID: process.env.GOOGLE_CLIENT_ID,
   // googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.SPACEGHOST_MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  redirectDomain: process.env.REDIRECT_DOMAIN,
-  discordBotToken: process.env.SPACEGHOST_DISCORD_BOT_TOKEN
+  cookieKey: process.env.SPACEGHOST_COOKIE_KEY,
+  redirectDomain: process.env.SPACEGHOST_REDIRECT_DOMAIN,
+  discordBotToken: process.env.SPACEGHOST_DISCORD_BOT_TOKEN,
+  discordClientId: process.env.SPACEGHOST_DISCORD_CLIENT_ID,
+  discordClientSecret: process.env.SPACEGHOST_DISCORD_CLIENT_SECRET,
+  giphyApiKey: process.env.SPACEGHOST_GIPHY_API_KEY
 }
