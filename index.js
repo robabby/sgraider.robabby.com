1643b2da00d7d1457274e07787951fd76ab30701
robabby <robabby23@gmail.com>
2018-05-30 22:00:09 -0500

    build

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ require('./routes/authRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Serve up production assets
-  app.use(express.static('client/public'));
+  app.use(express.static('client/build'));
 
   // Serve index.html if unrecognized route
   const path = require('path');
